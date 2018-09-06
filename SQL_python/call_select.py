894edcc9f916915c708ce1e055580980f9795641
Cartelle Fotsing <cartelle@MacBook-Pro-3.local>
2018-09-06 11:13:44 +0200

    read in to data Base

---
+++ SQL_python/call_select.py
@@ -0,0 +1,100 @@
+###############################################################################
+#
+# Copyright (C) 2014, Tavendo GmbH and/or collaborators. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+###############################################################################
+#import psycopg2
+import os
+import argparse
+import six
+import txaio
+import json
+import time
+import numpy 
+import sys
+import psycopg2
+from twisted.internet.defer import inlineCallbacks, returnValue
+from datetime import datetime
+#from twisted.internet.task import LoopingCall
+from twisted.internet import reactor
+from twisted.logger import Logger
+
+
+from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
+
+class Zaehler(ApplicationSession):
+
+    def onConnect(self):
+        self.log.info("Client connected")
+        self.join(self.config.realm, [u'anonymous'])
+
+    def onChallenge(self, challenge):
+        self.log.info("Challenge for method {authmethod} received", authmethod=challenge.method)
+        raise Exception("We haven't asked for authentication!")
+
+    @inlineCallbacks
+    def onJoin(self, details):
+        print('on join')
+       
+        
+       
+        x = yield self.call(u'repi.data.select',[20]) # 20 gives the number of values 
+        print(x)
+        # calculate mean and variance of x.vendor_name
+
+    #############
+
+    def onLeave(self, details):
+        self.log.info('session left: {}'.format(details))
+
+        self.disconnect()
+
+    def onDisconnect(self):
+        self.log.info('transport disconnected')
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    print ('parse command line parameters')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true', help='Enable debug output.')
+    parser.add_argument('--url', dest='url', type=six.text_type, default=u'ws://localhost:8080/ws', help='The router URL (default: "ws://104.199.76.81:8080/ws").')
+#    parser.add_argument('--router', type=six.text_type,default=u'ws://104.199.76.81:8080/ws',help='WAMP router URL.')
+ 
+#    parser.add_argument('--realm',type=six.text_type, default='realm1',help='WAMP router realm.')
+    parser.add_argument('--realm', dest='realm', type=six.text_type, default='realm1', help='The realm to join (default: "realm1").')
+
+    args = parser.parse_args()
+    print(args)
+    # now actually run a WAMP client using our session class ClientSession
+    runner = ApplicationRunner(url=args.url, realm=args.realm)
+    print(runner)
+    runner.run(Zaehler, auto_reconnect=True)
+
+    
